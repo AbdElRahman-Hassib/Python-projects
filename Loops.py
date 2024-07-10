1634bd7550f30e2c1f70779550330f5719514f0a
VNET\ahassib <abdelrahman.hassib@valeo.com>
2024-07-10 18:09:52 +0300

    Loops

--- Loops.py
+++ Loops.py
@@ -1,7 +1,24 @@
-x=3
-num=int(input("Enter a number"))
-while x<num:
-    print(x)
-    x+=3
+num=int(input("Enter a number:"))
+y,x =1,1
+while y<=num:
+    while x<=(num*2)-1:
+        if x==num or num-(y-1)<=x<=num+y-1:
+            print("*",end="")
+        else:
+            print(" ",end="")
+        x=x+1
+    print("")
+    x=1
+    y=y+1
 
-assert x>0 ,"error"
+y,x =1,1
+while y<=num:
+    while x<=(num*2)-1:
+        if x==num or (x+y<=(2*num)and y<=x):
+            print("*",end="")
+        else:
+            print(" ",end="")
+        x=x+1
+    print("")
+    x=1
+    y=y+1
